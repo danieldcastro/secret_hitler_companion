04d34a35ba1deab7f12eab3de8567df5715e90b0
Daniel Marciano <odanielmarciano@gmail.com>
2025-09-11 20:56:20 -0300

    feat: Adiciona suporte a múltiplos ambientes com configurações de lançamento para QA e PROD

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:secret_hitler_companion/app.dart';
 import 'package:secret_hitler_companion/core/dependencies/core_module.dart';
+import 'package:secret_hitler_companion/core/utils/flavors/flavor_env_utils.dart';
+import 'package:secret_hitler_companion/core/utils/flavors/flavors.dart';
 
 void main() {
+  Flavors.init(FlavorEnvUtils.env);
   runApp(ModularApp(module: CoreModule(), child: const App()));
 }
