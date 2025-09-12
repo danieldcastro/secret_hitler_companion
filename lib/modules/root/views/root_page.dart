7775cbb05a0ca79aa5e07c9d6e64a2272b70575d
Daniel Marciano <danielmarciano.ti@belenus.com.br>
2025-09-12 16:59:13 -0300

    feat: adiciona botão de navegação na parte inferior da tela Root

--- lib/modules/root/views/root_page.dart
+++ lib/modules/root/views/root_page.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:secret_hitler_companion/core/themes/app_colors.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({super.key});
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: AdvancedDrawer(drawer: Container(), child: RouterOutlet()),
+    bottomNavigationBar: ElevatedButton(
+      onPressed: () {},
+      child: const Text('Open Drawer'),
+    ),
+    body: AdvancedDrawer(
+      rtlOpening: true,
+      openScale: 0.9,
+      openRatio: 0.5,
+      backdropColor: AppColors.beige,
+      drawer: Container(),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: RouterOutlet(),
+      ),
+    ),
   );
 }
